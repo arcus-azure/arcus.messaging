f6db47357d9b40c62c152e4c30de0a2e37381a3f
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2019-12-02 16:57:02 +0100

    FEAT: initial health TCP probe (#16)
    * FEAT: initial health TCP probe
    
    * PR-SUG: add suggestions concerting TCP server
    
    * PR-ADD: add YAML build to run integration test
    
    * PR-SUG: use netcoreapp2.0
    
    * PR-FIX: use netcoreapp2.2
    
    * PR-SUG: use netcoreapp2.2
    
    * PR-FIX: update to netcore3.0
    
    * PR-FIX: use correct Dockerfile path
    
    * PR-FIX: correct Dockerfile
    
    * PR-FIX: use 'src' as build context
    
    * PR-FIX: update Dockerfile with netcore3.0
    
    * PR-FIX: use 'Arcus_Health_Port' in CI YAML build
    
    * PR-FIX: update integration test proj with netcoreapp3.0
    
    * PR-SUG: rename 'Server' > 'Listener' & add client ID

---
+++ src/Arcus.Messaging.Health/Tcp/TcpHealthListener.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GuardNet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Arcus.Messaging.Health.Tcp
+{
+    /// <summary>
+    /// Representing a TCP server as a background process to expose an endpoint where the <see cref="HealthReport"/> is being broadcasted.
+    /// </summary>
+    public class TcpHealthListener : BackgroundService
+    {
+        private const string TcpHealthPort = "ARCUS_HEALTH_PORT";
+
+        private readonly HealthCheckService _healthService;
+        private readonly TcpListener _listener;
+        private readonly ILogger<TcpHealthListener> _logger;
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TcpHealthListener"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration to control the hosting settings of the TCP server.</param>
+        /// <param name="healthService">The service to retrieve the current health of the application.</param>
+        /// <param name="logger">The logging implementation to write diagnostic messages during the running of the TCP server.</param>
+        public TcpHealthListener(IConfiguration configuration, HealthCheckService healthService, ILogger<TcpHealthListener> logger)
+        {
+            Guard.NotNull(configuration, nameof(configuration), "Requires a configuration implementation to retrieve hosting information for the TCP server");
+            Guard.NotNull(healthService, nameof(healthService), "Requires a health service to retrieve the current health status of the application");
+            Guard.NotNull(logger, nameof(logger), "Requires a logger implementation to write diagnostic messages during the running of the TCP server");
+
+            _healthService = healthService;
+            _logger = logger;
+
+            Port = GetConfiguredTcpPort(configuration);
+            _listener = new TcpListener(IPAddress.Any, Port);
+            _serializerSettings = CreateDefaultSerializerSettings();
+        }
+
+        private static int GetConfiguredTcpPort(IConfiguration configuration)
+        {
+            string tcpPortString = configuration[TcpHealthPort];
+            var tcpPort = 0;
+            Guard.For<ArgumentException>(
+                () => !Int32.TryParse(tcpPortString, out tcpPort), 
+                $"Requires a configuration implementation with a '{TcpHealthPort}' key containing a TCP port number");
+
+            return tcpPort;
+        }
+
+        private static JsonSerializerSettings CreateDefaultSerializerSettings()
+        {
+            var serializingSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.None, 
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var enumConverter = new StringEnumConverter { AllowIntegerValues = false };
+            serializingSettings.Converters.Add(enumConverter);
+
+            return serializingSettings;
+        }
+
+        /// <summary>
+        /// Gets the port on which the TCP health server is listening to.
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        /// Triggered when the application host is ready to start the service.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
+
+            try
+            {
+                _logger.LogTrace("Starting TCP server on port {Port}...", Port);
+                _listener.Start();
+                _logger.LogInformation("TCP server started on port {Port}!", Port);
+
+                return base.StartAsync(cancellationToken);
+            }
+            catch (Exception exception)
+            {
+                return Task.FromException(exception);
+            }
+        }
+
+        /// <summary>
+        /// This method is called when the <see cref="T:Microsoft.Extensions.Hosting.IHostedService" /> starts. The implementation should return a task that represents
+        /// the lifetime of the long running operation(s) being performed.
+        /// </summary>
+        /// <param name="stoppingToken">Triggered when <see cref="M:Microsoft.Extensions.Hosting.IHostedService.StopAsync(System.Threading.CancellationToken)" /> is called.</param>
+        /// <returns>A <see cref="T:System.Threading.Tasks.Task" /> that represents the long running operations.</returns>
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await AcceptConnectionAsync();
+            }
+        }
+
+        private async Task AcceptConnectionAsync()
+        {
+            _logger.LogTrace("Accepting TCP client on port {Port}...", Port);
+            using (TcpClient client = await _listener.AcceptTcpClientAsync())
+            {
+                _logger.LogInformation("TCP client accepted on port {Port}!", Port);
+                using (NetworkStream clientStream = client.GetStream())
+                {
+                    HealthReport report = await _healthService.CheckHealthAsync();
+                    string clientId = client.Client?.RemoteEndPoint?.ToString() ?? String.Empty;
+                    _logger.LogInformation("Return '{Status}' health report to client {ClientId}", report.Status, clientId);
+
+                    string serialized = JsonConvert.SerializeObject(report, _serializerSettings);
+                    byte[] response = Encoding.UTF8.GetBytes(serialized);
+                    clientStream.Write(response, 0, response.Length);
+
+                    clientStream.Close();
+                    client.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
+
+            try
+            {
+                _logger.LogTrace("Stopping TCP server on port {Port}...", Port);
+                _listener.Stop();
+                _logger.LogInformation("TCP server stopped on port {Port}!", Port);
+
+                return base.StopAsync(cancellationToken);
+            }
+            catch (Exception exception)
+            {
+                return Task.FromException(exception);
+            }
+        }
+    }
+}
---
+++ src/Arcus.Messaging.Tests.Integration/Health/TcpHealthCheckTests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Arcus.Messaging.Tests.Integration.Health
+{
+    [Trait("Category", "Integration")]
+    public class TcpHealthCheckTests : IntegrationTest
+    {
+        private readonly int _healthTcpPort;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TcpHealthCheckTests"/> class.
+        /// </summary>
+        public TcpHealthCheckTests(ITestOutputHelper testOutput) : base(testOutput)
+        {
+            _healthTcpPort = Configuration.GetValue<int>("Arcus:Health:Port");
+        }
+
+        [Fact]
+        public async Task TcpHealthServer_ProbeForHealthReport_ResponseHealthy()
+        {
+            // Arrange
+            using (var client = new TcpClient())
+            {
+                await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), _healthTcpPort);
+                using (NetworkStream clientStream = client.GetStream())
+                using (var reader = new StreamReader(clientStream))
+                {
+                    // Act
+                    string healthReport = await reader.ReadLineAsync();
+
+                    // Assert
+                    var report = JsonConvert.DeserializeObject<HealthReport>(healthReport);
+                    Assert.NotNull(report);
+                    Assert.Equal(HealthStatus.Healthy, report.Status);
+                }
+            }
+        }
+    }
+}
