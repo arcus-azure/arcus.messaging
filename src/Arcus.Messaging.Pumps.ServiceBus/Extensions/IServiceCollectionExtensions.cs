1a70190fd89b1133fee87f6825f134aa610948fc
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-12-05 18:19:22 +0100

    Rename extensions

--- src/Arcus.Messaging.Pumps.ServiceBus/Extensions/IServiceCollectionExtensions.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/Extensions/IServiceCollectionExtensions.cs
@@ -12,12 +12,27 @@ namespace Microsoft.Extensions.DependencyInjection
         /// </summary>
         /// <param name="services">Collection of services to use in the application</param>
         /// <returns>Collection of services to use in the application</returns>
-        public static IServiceCollection AddServiceBusMessageHandler<TMessageHandler>(this IServiceCollection services)
-            where TMessageHandler : class, IHostedService
+        public static IServiceCollection AddServiceBusQueueMessagePump<TMessagePump>(this IServiceCollection services)
+            where TMessagePump : class, IHostedService
         {
             Guard.NotNull(services, nameof(services));
 
-            services.AddHostedService<TMessageHandler>();
+            services.AddHostedService<TMessagePump>();
+
+            return services;
+        }
+
+        /// <summary>
+        ///     Adds a message handler to consume messages from Azure Service Bus health probing
+        /// </summary>
+        /// <param name="services">Collection of services to use in the application</param>
+        /// <returns>Collection of services to use in the application</returns>
+        public static IServiceCollection AddServiceBusTopicMessagePump<TMessagePump>(this IServiceCollection services)
+            where TMessagePump : class, IHostedService
+        {
+            Guard.NotNull(services, nameof(services));
+
+            services.AddHostedService<TMessagePump>();
 
             return services;
         }
