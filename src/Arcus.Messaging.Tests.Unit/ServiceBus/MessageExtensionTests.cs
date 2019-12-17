442578f089da2d03ce3052a3fbef7303bc3116c0
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-12-17 11:30:13 +0100

    Provide tests for GetTransactionId extension

---
+++ src/Arcus.Messaging.Tests.Unit/ServiceBus/MessageExtensionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arcus.Messaging.Abstractions;
+using Microsoft.Azure.ServiceBus;
+using Xunit;
+
+namespace Arcus.Messaging.Tests.Unit.ServiceBus
+{
+    [Trait("Category", "Unit")]
+    public class MessageExtensionTests
+    {
+        [Fact]
+        public void GetTransactionId_NoTransactionIdSpecified_ReturnsEmptyString()
+        {
+            // Arrange
+            var serviceBusMessage = new Message();
+
+            // Act
+            var transactionId = serviceBusMessage.GetTransactionId();
+
+            // Assert
+            Assert.Empty(transactionId);
+        }
+
+        [Fact]
+        public void GetTransactionId_TransactionIdSpecified_ReturnsCorrectTransactionId()
+        {
+            // Arrange
+            var expectedTransactionId = Guid.NewGuid().ToString();
+            var serviceBusMessage = new Message();
+            serviceBusMessage.UserProperties.Add(PropertyNames.TransactionId, expectedTransactionId);
+
+            // Act
+            var transactionId = serviceBusMessage.GetTransactionId();
+
+            // Assert
+            Assert.Equal(expectedTransactionId,transactionId);
+        }
+    }
+}
