49e975884b222e044a7026d1056bc19bf8094d97
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-12-17 16:04:36 +0100

    Don't allow negative or zero concurrent calls + add tests
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePumpOptions.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePumpOptions.cs
@@ -1,14 +1,27 @@
-﻿namespace Arcus.Messaging.Pumps.ServiceBus
+﻿using System;
+using GuardNet;
+
+namespace Arcus.Messaging.Pumps.ServiceBus
 {
     /// <summary>
     ///     Options to configure how the Azure Service Bus message pump works
     /// </summary>
     public class AzureServiceBusMessagePumpOptions
     {
+        private int? _maxConcurrentCalls;
+
         /// <summary>
         ///     Maximum concurrent calls to process messages
         /// </summary>
-        public int? MaxConcurrentCalls { get; set; }
+        public int? MaxConcurrentCalls
+        {
+            get => _maxConcurrentCalls;
+            set
+            {
+                Guard.For<ArgumentException>(() => value <= 0);
+                _maxConcurrentCalls = value;
+            }
+        }
 
         /// <summary>
         ///     Indication whether or not messages should be automatically marked as completed if no exceptions occured and
