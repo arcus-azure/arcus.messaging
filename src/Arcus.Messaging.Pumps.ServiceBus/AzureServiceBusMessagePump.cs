0810fc35845c5be9b492039528f2ad04d922b1cf
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2020-02-05 13:55:50 +0100

    PR-SUG: remove auto-delete in topic subscription

--- src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePump.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePump.cs
@@ -82,7 +82,6 @@ namespace Arcus.Messaging.Pumps.ServiceBus
             
             var subscriptionDescription = new SubscriptionDescription(serviceBusConnectionString.EntityPath, _subscriptionName)
             {
-                AutoDeleteOnIdle = TimeSpan.FromHours(1),
                 MaxDeliveryCount = 3,
                 UserMetadata = $"Subscription created by Arcus job: '{JobId}' to process Service Bus messages."
             };
