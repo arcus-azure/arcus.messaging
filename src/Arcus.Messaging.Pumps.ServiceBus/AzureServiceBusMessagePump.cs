160b2e0d065709ea29eee420509c7ffacd79ba29
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-08-03 13:04:56 +0200

    pr-tip: diff continuation

--- src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePump.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/AzureServiceBusMessagePump.cs
@@ -298,21 +298,26 @@ namespace Arcus.Messaging.Pumps.ServiceBus
             {
                 Logger.LogWarning("Unable to connect anymore to Azure Service Bus, trying to re-authenticate...");
                 Logger.LogTrace("Restarting Azure Service Bus...");
-                
-                await CloseMessageReceiverAsync();
-                using (var stopCancellationTokenSource = new CancellationTokenSource(Settings.Options.KeyRotationTimeout))
-                {
-                    await StopAsync(stopCancellationTokenSource.Token);
-                }
 
-                Logger.LogInformation("Azure Service Bus stopped!");
+                await CloseMessageReceiverAsync()
+                    .ContinueWith(async t =>
+                    {
+                        using (var stopCancellationTokenSource = new CancellationTokenSource(Settings.Options.KeyRotationTimeout))
+                        {
+                            await StopAsync(stopCancellationTokenSource.Token);
+                        }
 
-                using (var startCancellationTokenSource = new CancellationTokenSource(Settings.Options.KeyRotationTimeout))
-                {
-                    await StartAsync(startCancellationTokenSource.Token);
-                }
+                        Logger.LogInformation("Azure Service Bus stopped!");
+                    })
+                    .ContinueWith(async t =>
+                    {
+                        using (var startCancellationTokenSource = new CancellationTokenSource(Settings.Options.KeyRotationTimeout))
+                        {
+                            await StartAsync(startCancellationTokenSource.Token);
+                        }
 
-                Logger.LogInformation("Azure Service Bus restarted!");
+                        Logger.LogInformation("Azure Service Bus restarted!");
+                    });
             }
         }
 
