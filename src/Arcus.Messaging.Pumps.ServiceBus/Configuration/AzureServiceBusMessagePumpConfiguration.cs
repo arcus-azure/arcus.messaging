2cd04294d63aebbd3473887587545c9ce1c0a587
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-01-04 13:47:48 +0100

    Feature - log security event upon Azure Service Bus connection string retrieval (#142)

--- src/Arcus.Messaging.Pumps.ServiceBus/Configuration/AzureServiceBusMessagePumpConfiguration.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/Configuration/AzureServiceBusMessagePumpConfiguration.cs
@@ -12,25 +12,29 @@ namespace Arcus.Messaging.Pumps.ServiceBus.Configuration
         /// Initializes a new instance of the <see cref="AzureServiceBusMessagePumpConfiguration"/> class.
         /// </summary>
         public AzureServiceBusMessagePumpConfiguration(AzureServiceBusQueueMessagePumpOptions options)
+            : this((AzureServiceBusMessagePumpOptions) options)
         {
             Guard.NotNull(options, nameof(options));
-
-            MaxConcurrentCalls = options.MaxConcurrentCalls;
-            AutoComplete = options.AutoComplete;
-            JobId = options.JobId;
-            KeyRotationTimeout = options.KeyRotationTimeout;
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AzureServiceBusMessagePumpConfiguration"/> class.
         /// </summary>
-        public AzureServiceBusMessagePumpConfiguration(AzureServiceBusTopicMessagePumpOptions options)
+        public AzureServiceBusMessagePumpConfiguration(AzureServiceBusTopicMessagePumpOptions options) 
+            : this((AzureServiceBusMessagePumpOptions) options)
+        {
+            Guard.NotNull(options, nameof(options));
+
+            TopicSubscription = options.TopicSubscription;
+        }
+
+        private AzureServiceBusMessagePumpConfiguration(AzureServiceBusMessagePumpOptions options)
         {
             Guard.NotNull(options, nameof(options));
 
             MaxConcurrentCalls = options.MaxConcurrentCalls;
-            TopicSubscription = options.TopicSubscription;
             AutoComplete = options.AutoComplete;
+            EmitSecurityEvents = options.EmitSecurityEvents;
             JobId = options.JobId;
             KeyRotationTimeout = options.KeyRotationTimeout;
         }
@@ -55,6 +59,11 @@ namespace Arcus.Messaging.Pumps.ServiceBus.Configuration
         /// </summary>
         /// <remarks>When turned off, clients have to explicitly mark the messages as completed</remarks>
         internal bool AutoComplete { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag to indicate whether or not to emit security events during the lifetime of the message pump.
+        /// </summary>
+        internal bool EmitSecurityEvents { get; set; }
 
         /// <summary>
         /// Gets or sets the unique identifier for this background job to distinguish this job instance in a multi-instance deployment.
