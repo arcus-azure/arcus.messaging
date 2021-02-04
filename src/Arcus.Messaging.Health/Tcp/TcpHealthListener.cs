a35856d4cdc3788967934b4464eec980f0526e69
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-02-04 07:22:15 +0100

    Update src/Arcus.Messaging.Health/Tcp/TcpHealthListener.cs
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.Messaging.Health/Tcp/TcpHealthListener.cs
+++ src/Arcus.Messaging.Health/Tcp/TcpHealthListener.cs
@@ -124,7 +124,7 @@ namespace Arcus.Messaging.Health.Tcp
         {
             _logger.LogTrace("Starting TCP server on port {Port}...", Port);
             _listener.Start();
-            _logger.LogInformation("TCP server started on port {Port}", Port);
+            _logger.LogTrace("TCP server started on port {Port}", Port);
         }
 
         /// <summary>
