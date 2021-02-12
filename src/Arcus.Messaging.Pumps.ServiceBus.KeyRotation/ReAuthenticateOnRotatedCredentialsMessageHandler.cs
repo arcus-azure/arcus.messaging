7b12ca85ca9119ac488e36be887bdacb92a3bde9
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-02-12 11:07:02 +0100

    pr-trail: update with invalid operation exception when message pump is not found

--- src/Arcus.Messaging.Pumps.ServiceBus.KeyRotation/ReAuthenticateOnRotatedCredentialsMessageHandler.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus.KeyRotation/ReAuthenticateOnRotatedCredentialsMessageHandler.cs
@@ -7,6 +7,7 @@ using Arcus.Messaging.Pumps.Abstractions.MessageHandling;
 using CloudNative.CloudEvents;
 using GuardNet;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Rest.Azure;
 
 namespace Arcus.Messaging.Pumps.ServiceBus.KeyRotation
@@ -26,9 +27,25 @@ namespace Arcus.Messaging.Pumps.ServiceBus.KeyRotation
         /// </summary>
         /// <param name="targetConnectionStringKey">The secret key where the connection string credentials are located for the target message pump that needs to be auto-restarted.</param>
         /// <param name="messagePump">The message pump instance to restart when the message handler process an <see cref="SecretNewVersionCreated"/> event.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="messagePump"/> is <c>null</c>.</exception>
+        public ReAuthenticateOnRotatedCredentialsMessageHandler(
+            string targetConnectionStringKey, 
+            AzureServiceBusMessagePump messagePump)
+            : this(targetConnectionStringKey, messagePump, NullLogger<ReAuthenticateOnRotatedCredentialsMessageHandler>.Instance)
+        {
+        }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReAuthenticateOnRotatedCredentialsMessageHandler"/> class.
+        /// </summary>
+        /// <param name="targetConnectionStringKey">The secret key where the connection string credentials are located for the target message pump that needs to be auto-restarted.</param>
+        /// <param name="messagePump">The message pump instance to restart when the message handler process an <see cref="SecretNewVersionCreated"/> event.</param>
         /// <param name="logger">The logger instance to write diagnostic trace messages during the processing of the event.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="messagePump"/> or <paramref name="logger"/> is <c>null</c>.</exception>
-        public ReAuthenticateOnRotatedCredentialsMessageHandler(string targetConnectionStringKey, AzureServiceBusMessagePump messagePump, ILogger<ReAuthenticateOnRotatedCredentialsMessageHandler> logger)
+        public ReAuthenticateOnRotatedCredentialsMessageHandler(
+            string targetConnectionStringKey, 
+            AzureServiceBusMessagePump messagePump, 
+            ILogger<ReAuthenticateOnRotatedCredentialsMessageHandler> logger)
         {
             Guard.NotNullOrWhitespace(targetConnectionStringKey, nameof(targetConnectionStringKey), "Requires a non-blank secret key that points to the credentials that holds the connection string of the target message pump");
             Guard.NotNull(messagePump, nameof(messagePump), $"Requires an message pump instance to restart when the message handler process an {nameof(SecretNewVersionCreated)} event");
