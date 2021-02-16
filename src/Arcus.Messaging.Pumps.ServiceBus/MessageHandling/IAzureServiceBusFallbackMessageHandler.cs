3f91bc24c5d4a438ab24be39197d463a05a3ff63
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-02-16 09:41:27 +0100

    pr-fix: remove unecessary 'using'

--- src/Arcus.Messaging.Pumps.ServiceBus/MessageHandling/IAzureServiceBusFallbackMessageHandler.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/MessageHandling/IAzureServiceBusFallbackMessageHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Arcus.Messaging.Abstractions;
-using Arcus.Messaging.Pumps.Abstractions.MessageHandling;
 using Microsoft.Azure.ServiceBus;
 
 namespace Arcus.Messaging.Pumps.ServiceBus.MessageHandling
