5388049c624d305e19ed2317585cf3ca0d350399
Pedro Carmo <pedro.carmo@tecnico.ulisboa.pt>
2022-06-21 20:53:36 +0100

    chore: removed the AsServiceBusMessage to create a ServiceBusMessage instance in the Arcus.Messaging.ServiceBus.Core project - issue  #296

--- src/Arcus.Messaging.ServiceBus.Core/Extensions/ObjectExtensions.cs
+++ src/Arcus.Messaging.ServiceBus.Core/Extensions/ObjectExtensions.cs
@@ -15,45 +15,5 @@ namespace Microsoft.Azure.ServiceBus
     {
         private const string JsonContentType = "application/json";
 
-        /// <summary>
-        ///     Creates an Azure Service Bus Message for a message body
-        /// </summary>
-        /// <param name="messageBody">Body of the Service Bus message to process</param>
-        /// <param name="operationId">Unique identifier that spans one operation end-to-end</param>
-        /// <param name="transactionId">Unique identifier that spans one or more operations and are considered a transaction/session</param>
-        /// <param name="encoding">Encoding to use during serialization. Defaults to UTF8</param>
-        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="messageBody"/> is <c>null</c>.</exception>
-        /// <returns>an <see cref="ServiceBusMessage"/>.</returns>
-        [Obsolete("Use the " + nameof(ServiceBusMessageBuilder) + " instead to create " + nameof(ServiceBusMessage) + " instances")]
-        public static ServiceBusMessage AsServiceBusMessage(this object messageBody, string operationId = null, string transactionId = null, Encoding encoding = null)
-        { 
-            Guard.NotNull(messageBody, nameof(messageBody), "Requires a message body to create an Azure Service Bus message this message body");
-
-            encoding = encoding ?? Encoding.UTF8;
-
-            string serializedMessageBody = JsonConvert.SerializeObject(messageBody);
-            byte[] rawMessage = encoding.GetBytes(serializedMessageBody);
-           
-            var serviceBusMessage = new ServiceBusMessage(rawMessage)
-            {
-                ApplicationProperties =
-                {
-                    { PropertyNames.ContentType, JsonContentType },
-                    { PropertyNames.Encoding, encoding.WebName }
-                }
-            };
-            
-            if (operationId != null)
-            {
-                serviceBusMessage.CorrelationId = operationId;
-            }
-
-            if (string.IsNullOrWhiteSpace(transactionId) == false)
-            {
-                serviceBusMessage.ApplicationProperties.Add(PropertyNames.TransactionId, transactionId);
-            }
-
-            return serviceBusMessage;
-        }
     }
 }
