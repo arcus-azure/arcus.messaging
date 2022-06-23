82f0b22626e687f7d06f949541e41a4cfffae454
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-06-23 10:05:22 +0200

    feat: make message correlation enrichment properties configurable (#306)
    * feat: make message correlation enrichment properties configurable
    
    * pr-fix: update w telemetry options in general message router
    
    * pr-sug: use 'correlation enricher' as option name

---
+++ src/Arcus.Messaging.Abstractions/MessageHandling/MessageCorrelationEnricherOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Arcus.Messaging.Abstractions.Telemetry;
+using Arcus.Observability.Telemetry.Serilog.Enrichers.Configuration;
+using GuardNet;
+
+namespace Arcus.Messaging.Abstractions.MessageHandling
+{
+    /// <summary>
+    /// Represents the consumer configurable options model to change the behavior of the Serilog <see cref="MessageCorrelationInfoEnricher"/>.
+    /// </summary>
+    public class MessageCorrelationEnricherOptions : CorrelationInfoEnricherOptions
+    {
+        private string _cycleIdPropertyName = "CycleId";
+
+        /// <summary>
+        /// Gets or sets the property name to enrich the log event with the correlation information cycle ID.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="value"/> is blank.</exception>
+        public string CycleIdPropertyName
+        {
+            get => _cycleIdPropertyName;
+            set
+            {
+                Guard.NotNullOrWhitespace(value, nameof(value), "Requires a non-blank property name to enrich the log event with the correlation information cycle ID");
+                _cycleIdPropertyName = value;
+            }
+        }
+    }
+}
--- src/Arcus.Messaging.Abstractions/MessageHandling/MessageRouterOptions.cs
+++ src/Arcus.Messaging.Abstractions/MessageHandling/MessageRouterOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace Arcus.Messaging.Abstractions.MessageHandling
+﻿using Arcus.Messaging.Abstractions.Telemetry;
+using Arcus.Observability.Telemetry.Serilog.Enrichers.Configuration;
+
+namespace Arcus.Messaging.Abstractions.MessageHandling
 {
     /// <summary>
     /// Represents the consumer-configurable options to change the behavior of the <see cref="MessageRouter"/>.
@@ -14,5 +17,10 @@
         /// Gets the options to control the correlation information upon the receiving of messages in the message router.
         /// </summary>
         public MessageCorrelationOptions Correlation { get; } = new MessageCorrelationOptions();
+
+        /// <summary>
+        /// Gets the options to control the Serilog <see cref="MessageCorrelationInfoEnricher"/> when the incoming message is routed via the message router.
+        /// </summary>
+        public MessageCorrelationEnricherOptions CorrelationEnricher { get; } = new MessageCorrelationEnricherOptions();
     }
 }
