a847c0d4c178ac7576cb8cdccd1fbf15ec400b8a
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-10-25 08:53:50 +0200

    chore: remove order publishing duplication

--- src/Arcus.Messaging.Tests.Workers.ServiceBus/MessageHandlers/OrdersServiceBusFallbackMessageHandler.cs
+++ src/Arcus.Messaging.Tests.Workers.ServiceBus/MessageHandlers/OrdersServiceBusFallbackMessageHandler.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Net.Mime;
-using System.Text;
+﻿using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Arcus.EventGrid.Publishing.Interfaces;
 using Arcus.Messaging.Abstractions;
 using Arcus.Messaging.Abstractions.ServiceBus;
 using Arcus.Messaging.Abstractions.ServiceBus.MessageHandling;
-using Arcus.Messaging.Tests.Core.Events.v1;
 using Arcus.Messaging.Tests.Core.Messages.v1;
 using Azure.Messaging.ServiceBus;
-using CloudNative.CloudEvents;
 using GuardNet;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -55,37 +51,7 @@ namespace Arcus.Messaging.Tests.Workers.MessageHandlers
             string messageBody = Encoding.UTF8.GetString(message.Body);
             var order = JsonConvert.DeserializeObject<Order>(messageBody);
 
-            _logger.LogInformation("Processing order {OrderId} for {OrderAmount} units of {OrderArticle} bought by {CustomerFirstName} {CustomerLastName}",
-                                   order.Id, order.Amount, order.ArticleNumber, order.Customer.FirstName, order.Customer.LastName);
-
-            await PublishEventToEventGridAsync(order, correlationInfo.OperationId, correlationInfo);
-
-            _logger.LogInformation("Order {OrderId} processed", order.Id);
-        }
-
-        private async Task PublishEventToEventGridAsync(Order orderMessage, string operationId, MessageCorrelationInfo correlationInfo)
-        {
-            var eventData = new OrderCreatedEventData(
-                orderMessage.Id,
-                orderMessage.Amount,
-                orderMessage.ArticleNumber,
-                $"{orderMessage.Customer.FirstName} {orderMessage.Customer.LastName}",
-                correlationInfo);
-
-            var orderCreatedEvent = new CloudEvent(
-                CloudEventsSpecVersion.V1_0,
-                "OrderCreatedEvent",
-                new Uri("http://test-host"),
-                operationId,
-                DateTime.UtcNow)
-            {
-                Data = eventData,
-                DataContentType = new ContentType("application/json")
-            };
-
-            await _eventGridPublisher.PublishAsync(orderCreatedEvent);
-
-            _logger.LogInformation("Event {EventId} was published with subject {EventSubject}", orderCreatedEvent.Id, orderCreatedEvent.Subject);
+            await _eventGridPublisher.PublishOrderAsync(order, correlationInfo, _logger);
         }
     }
 }
