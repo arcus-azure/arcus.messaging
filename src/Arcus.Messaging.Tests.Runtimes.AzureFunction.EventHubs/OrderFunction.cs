840fb0fe4abc2da31e5159171f87ef4bc4bee89a
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-11-03 10:00:34 +0100

    feat: add simpler w3c correlation support for az func eventhubs

--- src/Arcus.Messaging.Tests.Runtimes.AzureFunction.EventHubs/OrderFunction.cs
+++ src/Arcus.Messaging.Tests.Runtimes.AzureFunction.EventHubs/OrderFunction.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,7 +6,6 @@ using Arcus.Messaging.Abstractions;
 using Arcus.Messaging.Abstractions.EventHubs;
 using Arcus.Messaging.Abstractions.EventHubs.MessageHandling;
 using Azure.Messaging.EventHubs;
-using Microsoft.ApplicationInsights;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -17,50 +14,43 @@ namespace Arcus.Messaging.Tests.Runtimes.AzureFunction.EventHubs
     public class OrderFunction
     {
         private readonly IAzureEventHubsMessageRouter _messageRouter;
-        private readonly TelemetryClient _telemetryClient;
         private readonly ILogger _logger;
 
         public OrderFunction(
             IAzureEventHubsMessageRouter messageRouter,
-            TelemetryClient telemetryClient,
             ILoggerFactory loggerFactory)
         {
             _messageRouter = messageRouter;
-            _telemetryClient = telemetryClient;
             _logger = loggerFactory.CreateLogger<OrderFunction>();
         }
 
         [Function("orders")]
         public async Task Run(
             [EventHubTrigger("orders-az-func-docker", Connection = "EventHubsConnectionString")] string[] messages,
-            Dictionary<string, JsonElement>[] propertiesArray)
+            Dictionary<string, JsonElement>[] propertiesArray,
+            FunctionContext executionContext)
         {
             _logger.LogInformation($"First Event Hubs triggered message: {messages[0]}");
 
-            for (int i = 0; i < messages.Length; i++)
+            for (var i = 0; i < messages.Length; i++)
             {
                 string message = messages[i];
-                IDictionary<string, object> properties = GetUserProperties(propertiesArray, i);
+                Dictionary<string, JsonElement> properties = propertiesArray[i];
+                
                 EventData eventData = CreateEventData(message, properties);
-                var context = AzureEventHubsMessageContext.CreateFrom(eventData, "<namespace>", "$Default", "<eventhubs-name>");
+                AzureEventHubsMessageContext messageContext = eventData.GetMessageContext("<namespace>", "$Default", "<eventhubs-name>");
 
-                (string transactionId, string operationParentId) = properties.GetTraceParent();
-                using (var result = MessageCorrelationResult.Create(_telemetryClient, transactionId, operationParentId))
+                using (MessageCorrelationResult result = executionContext.GetCorrelationInfo(properties))
                 {
-                    await _messageRouter.RouteMessageAsync(eventData, context, result.CorrelationInfo, CancellationToken.None);
+                    await _messageRouter.RouteMessageAsync(eventData, messageContext, result.CorrelationInfo, CancellationToken.None);
                 }
             }
         }
 
-        private static Dictionary<string, object> GetUserProperties(Dictionary<string, JsonElement>[] propertiesArray, int i)
-        {
-            return propertiesArray[i].ToDictionary(item => item.Key, item => (object) item.Value.GetString());
-        }
-
-        private static EventData CreateEventData(string message, IDictionary<string, object> properties)
+        private static EventData CreateEventData(string message, IDictionary<string, JsonElement> properties)
         {
             var data = new EventData(message);
-            foreach (KeyValuePair<string, object> property in properties)
+            foreach (KeyValuePair<string, JsonElement> property in properties)
             {
                 data.Properties.Add(property.Key, property.Value.ToString());
             }
