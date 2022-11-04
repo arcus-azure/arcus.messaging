6ff82e57bf90c1fc45bcb22e86bde78055bc04a5
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-11-04 08:12:28 +0100

    pr-fix: add unit tests for functions host builder extensions

--- src/Arcus.Messaging.AzureFunctions.EventHubs/Extensions/IFunctionsHostBuilderExtensions.cs
+++ src/Arcus.Messaging.AzureFunctions.EventHubs/Extensions/IFunctionsHostBuilderExtensions.cs
@@ -43,5 +43,46 @@ namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
             return builder.Services.AddSingleton<AzureFunctionsInProcessMessageCorrelation>()
                           .AddEventHubsMessageRouting(configureOptions);
         }
+
+        /// <summary>
+        ///     Adds an <see cref="IAzureEventHubsMessageRouter"/> implementation
+        ///     to route the incoming messages through registered <see cref="IAzureEventHubsMessageHandler{TMessage}"/> instances.
+        /// </summary>
+        /// <param name="builder">The collection of services to add the router to.</param>
+        /// <param name="implementationFactory">The function to create the <typeparamref name="TMessageRouter"/> implementation.</param>
+        /// <typeparam name="TMessageRouter">The type of the <see cref="IAzureEventHubsMessageRouter"/> implementation.</typeparam>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or <paramref name="implementationFactory"/> is <c>null</c>.</exception>
+        public static EventHubsMessageHandlerCollection AddEventHubsMessageRouting<TMessageRouter>(
+            this IFunctionsHostBuilder builder,
+            Func<IServiceProvider, TMessageRouter> implementationFactory)
+            where TMessageRouter : IAzureEventHubsMessageRouter
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a functions host builder to add the Azure EventHubs message router");
+            Guard.NotNull(implementationFactory, nameof(implementationFactory), "Requires a function to create the Azure EventHubs message router");
+
+            return AddEventHubsMessageRouting(builder, (provider, options) => implementationFactory(provider), configureOptions: null);
+        }
+
+        /// <summary>
+        ///     Adds an <see cref="IAzureEventHubsMessageRouter"/> implementation
+        ///     to route the incoming messages through registered <see cref="IAzureEventHubsMessageHandler{TMessage}"/> instances.
+        /// </summary>
+        /// <param name="builder">The collection of services to add the router to.</param>
+        /// <param name="implementationFactory">The function to create the <typeparamref name="TMessageRouter"/> implementation.</param>
+        /// <param name="configureOptions">The function to configure the options that change the behavior of the router.</param>
+        /// <typeparam name="TMessageRouter">The type of the <see cref="IAzureEventHubsMessageRouter"/> implementation.</typeparam>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or <paramref name="implementationFactory"/> is <c>null</c>.</exception>
+        public static EventHubsMessageHandlerCollection AddEventHubsMessageRouting<TMessageRouter>(
+            this IFunctionsHostBuilder builder,
+            Func<IServiceProvider, AzureEventHubsMessageRouterOptions, TMessageRouter> implementationFactory,
+            Action<AzureEventHubsMessageRouterOptions> configureOptions)
+            where TMessageRouter : IAzureEventHubsMessageRouter
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a functions host builder to add the Azure EventHubs message router");
+            Guard.NotNull(implementationFactory, nameof(implementationFactory), "Requires a function to create the Azure EventHubs message router");
+
+            return builder.Services.AddSingleton<AzureFunctionsInProcessMessageCorrelation>()
+                          .AddEventHubsMessageRouting(implementationFactory, configureOptions);
+        }
     }
 }
---
+++ src/Arcus.Messaging.Tests.Unit/EventHubs/AzureFunctions/IFunctionsHostBuilderExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arcus.Messaging.Abstractions.EventHubs.MessageHandling;
+using Arcus.Messaging.AzureFunctions.EventHubs;
+using Arcus.Messaging.Tests.Unit.EventHubs.Fixture;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Arcus.Messaging.Tests.Unit.EventHubs.AzureFunctions
+{
+    // ReSharper disable once InconsistentNaming
+    public class IFunctionsHostBuilderExtensionsTests
+    {
+        [Fact]
+        public void AddEventHubsMessageRouting_WithoutOptions_RegistersRouter()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(b => b.Services).Returns(services);
+
+            // Act
+            builder.Object.AddEventHubsMessageRouting();
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IAzureEventHubsMessageRouter>());
+            Assert.NotNull(provider.GetService<AzureFunctionsInProcessMessageCorrelation>());
+        }
+
+        [Fact]
+        public void AddEventHubsRouting_WithOptions_RegistersRouter()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(b => b.Services).Returns(services);
+
+            // Act
+            builder.Object.AddEventHubsMessageRouting(configureOptions: options => { });
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            Assert.NotNull(provider.GetService<IAzureEventHubsMessageRouter>());
+            Assert.NotNull(provider.GetService<AzureFunctionsInProcessMessageCorrelation>());
+        }
+
+        [Fact]
+        public void AddEventHubsRoutingT_WithoutOptions_RegistersRouter()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(b => b.Services).Returns(services);
+
+            // Act
+            builder.Object.AddEventHubsMessageRouting(
+                serviceProvider => new TestAzureEventHubsMessageRouter(serviceProvider));
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            var messageRouter = provider.GetService<IAzureEventHubsMessageRouter>();
+            Assert.NotNull(messageRouter);
+            Assert.IsType<TestAzureEventHubsMessageRouter>(messageRouter);
+            Assert.NotNull(provider.GetService<AzureFunctionsInProcessMessageCorrelation>());
+        }
+
+        [Fact]
+        public void AddEventHubsRoutingT_WithOptions_RegistersRouter()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new Mock<IFunctionsHostBuilder>();
+            builder.Setup(b => b.Services).Returns(services);
+
+            // Act
+            builder.Object.AddEventHubsMessageRouting(
+                (serviceProvider, options) => new TestAzureEventHubsMessageRouter(serviceProvider),
+                configureOptions: null);
+
+            // Assert
+            IServiceProvider provider = services.BuildServiceProvider();
+            var messageRouter = provider.GetService<IAzureEventHubsMessageRouter>();
+            Assert.NotNull(messageRouter);
+            Assert.IsType<TestAzureEventHubsMessageRouter>(messageRouter);
+            Assert.NotNull(provider.GetService<AzureFunctionsInProcessMessageCorrelation>());
+        }
+    }
+}
