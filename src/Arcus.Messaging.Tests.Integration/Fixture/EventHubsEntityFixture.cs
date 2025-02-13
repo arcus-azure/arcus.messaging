7d294284126e67e6a28519fc23dc5fdef97c89b4
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-02-13 06:46:54 +0100

    chore(remove): event-hubs pump project

---
+++ src/Arcus.Messaging.Tests.Integration/Fixture/EventHubsEntityFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Arcus.Testing;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Arcus.Messaging.Tests.Integration.Fixture
+{
+    public class EventHubsEntityFixture : IAsyncLifetime
+    {
+        private TemporaryEventHubEntity _hub;
+
+        public string HubName { get; } = $"hub-{Guid.NewGuid()}";
+
+        public async Task InitializeAsync()
+        {
+            var config = TestConfig.Create();
+            _hub = await TemporaryEventHubEntity.CreateAsync(HubName, config.GetEventHubs(), NullLogger.Instance);
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _hub.DisposeAsync();
+        }
+    }
+}
