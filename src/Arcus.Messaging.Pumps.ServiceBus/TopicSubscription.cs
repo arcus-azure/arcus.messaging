b311576392163e47135793f6768778785477e55a
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-03-13 09:09:56 +0100

    Update src/Arcus.Messaging.Pumps.ServiceBus/TopicSubscription.cs
    Co-authored-by: Frederik Gheysels <frederik.gheysels@telenet.be>

--- src/Arcus.Messaging.Pumps.ServiceBus/TopicSubscription.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/TopicSubscription.cs
@@ -5,7 +5,7 @@ namespace Arcus.Messaging.Pumps.ServiceBus
     /// <summary>
     /// Represents the option to control the topic subscription existence during the lifecycle of the <see cref="AzureServiceBusMessagePump"/>.
     /// </summary>
-    [Obsolete("Will be removed in v3.0 as automatic Azure Service bus topic subscriptions will not be supported anymore")]
+    [Obsolete("Will be removed in v3.0 as automatic Azure Service bus topic subscription creation and deletion will not be supported anymore")]
     public enum TopicSubscription
     {
         /// <summary>
