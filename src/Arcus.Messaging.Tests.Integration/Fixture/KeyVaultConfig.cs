3effbe10d7667b5cd7e5be4b5604495832280e15
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-03-20 07:30:18 +0100

    chore(testing): remove unused key rotation test configuration

--- src/Arcus.Messaging.Tests.Integration/Fixture/KeyVaultConfig.cs
+++ src/Arcus.Messaging.Tests.Integration/Fixture/KeyVaultConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Arcus.Testing;
 using Azure.Security.KeyVault.Secrets;
-using GuardNet;
 
 namespace Arcus.Messaging.Tests.Integration.Fixture
 {
@@ -11,25 +10,6 @@ namespace Arcus.Messaging.Tests.Integration.Fixture
     public class KeyVaultConfig
     {
         private readonly ServicePrincipal _servicePrincipal;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="KeyVaultConfig" /> class.
-        /// </summary>
-        /// <param name="vaultUri">The URI referencing the Azure Key Vault instance.</param>
-        /// <param name="secretName">The name of the secret in the Azure Key Vault instance.</param>
-        /// <param name="secretNewVersionCreated">The event endpoint of the Azure Key Vault 'Secret new version created' event.</param>
-        /// <exception cref="ArgumentException">Thrown when the <paramref name="vaultUri"/> or <paramref name="secretName"/> is blank.</exception>
-        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="secretNewVersionCreated"/> is <c>null</c>.</exception>
-        public KeyVaultConfig(string vaultUri, string secretName, KeyVaultEventEndpoint secretNewVersionCreated)
-        {
-            Guard.NotNullOrWhitespace(vaultUri, nameof(vaultUri));
-            Guard.NotNullOrWhitespace(secretName, nameof(secretName));
-            Guard.NotNull(secretNewVersionCreated, nameof(secretNewVersionCreated));
-
-            VaultUri = vaultUri;
-            SecretName = secretName;
-            SecretNewVersionCreated = secretNewVersionCreated;
-        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="KeyVaultConfig" /> class.
@@ -44,16 +24,6 @@ namespace Arcus.Messaging.Tests.Integration.Fixture
         /// Gets the URI referencing the Azure Key Vault instance.
         /// </summary>
         public string VaultUri { get; }
-
-        /// <summary>
-        /// Gets the name of the secret in the Azure Key Vault instance.
-        /// </summary>
-        public string SecretName { get; }
-
-        /// <summary>
-        /// Gets the endpoint where Azure Key Vault events will be available, including 'Secret new version created' event.
-        /// </summary>
-        public KeyVaultEventEndpoint SecretNewVersionCreated { get; }
 
         public SecretClient GetClient()
         {
