d59441e3df8e07fd639fef0f0ca5de9f87917b3d
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-03-27 20:36:53 +0100

    chore(observability): deprecate message correlation accessor (#526)

--- src/Arcus.Messaging.Abstractions/IMessageCorrelationInfoAccessor.cs
+++ src/Arcus.Messaging.Abstractions/IMessageCorrelationInfoAccessor.cs
@@ -1,4 +1,5 @@
-﻿using Arcus.Observability.Correlation;
+﻿using System;
+using Arcus.Observability.Correlation;
 
 namespace Arcus.Messaging.Abstractions
 {
@@ -6,6 +7,7 @@ namespace Arcus.Messaging.Abstractions
     /// Represents a marker interface for an <see cref="ICorrelationInfoAccessor{TCorrelationInfo}"/> implementation using the messaging <see cref="MessageCorrelationInfo"/>.
     /// </summary>
     /// <seealso cref="ICorrelationInfoAccessor{TCorrelationInfo}"/>
+    [Obsolete("Will be moved in v3.0 outside the 'Abstractions' library in a separate Serilog-specific library, see the v3.0 migration guide for more information")]
     public interface IMessageCorrelationInfoAccessor : ICorrelationInfoAccessor<MessageCorrelationInfo>
     {
     }
