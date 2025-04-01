5c8de21fd6534b0f83c991df92107a4fa605add4
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-04-01 10:43:11 +0200

    feat(az.sb.telemetry): add serilog message correlation scope implementation

---
+++ src/Arcus.Messaging.ServiceBus.Telemetry.Serilog/Extensions/SerilogServiceBusMessageHandlerCollectionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Arcus.Messaging.Abstractions.ServiceBus.Telemetry;
+using Arcus.Messaging.ServiceBus.Telemetry.Serilog;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable once CheckNamespace
+namespace Arcus.Messaging.Abstractions.ServiceBus.MessageHandling
+{
+    /// <summary>
+    /// Extensions on the <see cref="ServiceBusMessageHandlerCollection"/> to use Serilog to track Azure Service bus request telemetry to Azure Application Insights.
+    /// </summary>
+    public static class SerilogServiceBusMessageHandlerCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Serilog as message correlation system to track Azure Service bus request telemetry in Azure Application Insights.
+        /// </summary>
+        /// <remarks>
+        ///     Make sure that the <see cref="TelemetryClient"/> is available in the application services, by registering the Azure Application Insights services.
+        /// </remarks>
+        /// <param name="collection">The application services to add the Serilog correlation system to.</param>
+        public static ServiceBusMessageHandlerCollection WithServiceBusSerilogRequestTracking(
+            this ServiceBusMessageHandlerCollection collection)
+        {
+            return WithServiceBusSerilogRequestTracking(collection, configureOptions: null);
+        }
+
+        /// <summary>
+        /// Adds Serilog as message correlation system to track Azure Service bus request telemetry in Azure Application Insights.
+        /// </summary>
+        /// <remarks>
+        ///     Make sure that the <see cref="TelemetryClient"/> is available in the application services, by registering the Azure Application Insights services.
+        /// </remarks>
+        /// <param name="collection">The application services to add the Serilog correlation system to.</param>
+        /// <param name="configureOptions">The additional function to manipulate the </param>
+        public static ServiceBusMessageHandlerCollection WithServiceBusSerilogRequestTracking(
+            this ServiceBusMessageHandlerCollection collection,
+            Action<SerilogMessageCorrelationOptions> configureOptions)
+        {
+            collection.Services.AddSingleton<IServiceBusMessageCorrelationScope>(provider =>
+            {
+                var options = new SerilogMessageCorrelationOptions();
+                configureOptions?.Invoke(options);
+
+                return new SerilogServiceBusMessageCorrelationScope(
+                    provider.GetRequiredService<TelemetryClient>(),
+                    options,
+                    provider.GetRequiredService<ILogger<SerilogServiceBusMessageCorrelationScope>>());
+            });
+
+            return collection;
+        }
+    }
+}
---
+++ src/Arcus.Messaging.ServiceBus.Telemetry.Serilog/SerilogMessageCorrelationInfoEnricher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Arcus.Messaging.Abstractions;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Arcus.Messaging.ServiceBus.Telemetry.Serilog
+{
+    /// <summary>
+    /// Logger enrichment of the <see cref="MessageCorrelationInfo" /> model.
+    /// </summary>
+    internal class SerilogMessageCorrelationInfoEnricher : ILogEventEnricher
+    {
+        private readonly MessageCorrelationInfo _messageCorrelationInfo;
+        private readonly SerilogMessageCorrelationEnricherOptions _options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerilogMessageCorrelationInfoEnricher" /> class.
+        /// </summary>
+        /// <param name="messageCorrelationInfo">The current message correlation instance that should be enriched on the log events.</param>
+        /// <param name="options">The additional options to change the behavior of the message correlation enrichment on the log events.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="messageCorrelationInfo"/> or <paramref name="options"/> is <c>null</c>.</exception>
+        internal SerilogMessageCorrelationInfoEnricher(MessageCorrelationInfo messageCorrelationInfo, SerilogMessageCorrelationEnricherOptions options)
+        {
+            _messageCorrelationInfo = messageCorrelationInfo ?? throw new ArgumentNullException(nameof(messageCorrelationInfo));
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        /// <summary>
+        /// Enrich the log event.
+        /// </summary>
+        /// <param name="logEvent">The log event to enrich.</param>
+        /// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            AddPropertyIfAbsent(_options.OperationIdPropertyName, _messageCorrelationInfo.OperationId, logEvent, propertyFactory);
+            AddPropertyIfAbsent(_options.TransactionIdPropertyName, _messageCorrelationInfo.TransactionId, logEvent, propertyFactory);
+            AddPropertyIfAbsent(_options.OperationParentIdPropertyName, _messageCorrelationInfo.OperationParentId, logEvent, propertyFactory);
+            AddPropertyIfAbsent(_options.CycleIdPropertyName, _messageCorrelationInfo.CycleId, logEvent, propertyFactory);
+        }
+
+        private static void AddPropertyIfAbsent(string propertyName, string propertyValue, LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (!string.IsNullOrEmpty(propertyValue))
+            {
+                LogEventProperty property = propertyFactory.CreateProperty(propertyName, propertyValue);
+                logEvent.AddPropertyIfAbsent(property);
+            }
+        }
+    }
+}
