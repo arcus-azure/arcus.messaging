9c7fc84d1888e913e60080ad59e6456342676e7f
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-06-17 09:55:28 +0200

    chore(az.sb): remove deprecated hierarchical correlation usage in az sb msg pump (#594)

--- src/Arcus.Messaging.Abstractions/MessageHandling/MessageCorrelationOptions.cs
+++ src/Arcus.Messaging.Abstractions/MessageHandling/MessageCorrelationOptions.cs
@@ -10,57 +10,9 @@ namespace Arcus.Messaging.Abstractions.MessageHandling
     [Obsolete("Will be removed in v3.0 as W3C will be the only supported correlation format")]
     public class MessageCorrelationOptions
     {
-        [Obsolete("Will be removed in v3.0")]
-        private string _transactionIdPropertyName = PropertyNames.TransactionId,
-                       _operationParentIdPropertyName = PropertyNames.OperationParentId;
-
         /// <summary>
         /// Gets or sets the message correlation format of the received message.
         /// </summary>
         public MessageCorrelationFormat Format { get; set; } = MessageCorrelationFormat.W3C;
-
-        /// <summary>
-        /// Gets or sets the name of the message property to retrieve the transaction ID.
-        /// </summary>
-        /// <remarks>
-        ///     Only used when the <see cref="Format"/> is set to <see cref="MessageCorrelationFormat.Hierarchical"/>.
-        /// </remarks>
-        /// <exception cref="ArgumentException">Thrown when the <paramref name="value"/> is blank.</exception>
-        [Obsolete("Will be removed in v3.0 as the property name is only used in the deprecated 'Hierarchical' correlation format")]
-        public string TransactionIdPropertyName
-        {
-            get => _transactionIdPropertyName;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Requires a non-blank property name for the transaction ID", nameof(value));
-                }
-
-                _transactionIdPropertyName = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the name of the message property to retrieve the operation parent ID.
-        /// </summary>
-        /// <remarks>
-        ///     Only used when the <see cref="Format"/> is set to <see cref="MessageCorrelationFormat.Hierarchical"/>.
-        /// </remarks>
-        /// <exception cref="ArgumentException">Thrown when the <paramref name="value"/> is blank.</exception>
-        [Obsolete("Will be removed in v3.0 as the property name is only used in the deprecated 'Hierarchical' correlation format")]
-        public string OperationParentIdPropertyName
-        {
-            get => _operationParentIdPropertyName;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Requires a non-blank property name for the operation parent ID", nameof(value));
-                }
-
-                _operationParentIdPropertyName = value;
-            }
-        }
     }
 }
