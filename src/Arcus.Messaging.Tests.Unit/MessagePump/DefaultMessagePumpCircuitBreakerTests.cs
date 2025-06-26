09e5fbbf7465c73136de92be9ccfba7eb6050d02
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-06-26 08:38:50 +0200

    chore(deps): upgrade to xunit v3

--- src/Arcus.Messaging.Tests.Unit/MessagePump/DefaultMessagePumpCircuitBreakerTests.cs
+++ src/Arcus.Messaging.Tests.Unit/MessagePump/DefaultMessagePumpCircuitBreakerTests.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Arcus.Messaging.Tests.Unit.MessagePump
 {
