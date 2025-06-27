3bc14b91bf810c9ee90f06e8915edb9222fe66e8
Frederik Gheysels <frederik.gheysels@codit.eu>
2025-06-27 15:16:29 +0200

    no readonly props

--- src/Arcus.Messaging.Pumps.ServiceBus/Configuration/AzureServiceBusSessionMessagePumpOptions.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/Configuration/AzureServiceBusSessionMessagePumpOptions.cs
@@ -37,18 +37,18 @@ namespace Arcus.Messaging.Pumps.ServiceBus.Configuration
         /// Gets the maximum number of calls to the callback the processor will initiate per session.
         /// Thus the total number of callbacks will be equal to MaxConcurrentSessions * MaxConcurrentCallsPerSession. The default value is 1.
         /// </summary>
-        public int MaxConcurrentCallsPerSession { get; private set; }
+        public int MaxConcurrentCallsPerSession { get; set; }
 
         /// <summary>
         /// Gets the maximum number of sessions that will be processed concurrently by the processor. The default value is 8.
         /// </summary>
-        public int MaxConcurrentSessions { get; private set; }
+        public int MaxConcurrentSessions { get; set; }
 
         /// <summary>
         /// Gets the maximum amount of time to wait for a message to be received for the currently active session. After this time has elapsed, the processor will close the session and attempt to process another session.
         /// The default value is 60 seconds.
         /// </summary>
-        public TimeSpan SessionIdleTimeout { get; private set; }
+        public TimeSpan SessionIdleTimeout { get; set; }
 
         /// <summary>
         /// Gets the consumer-configurable options to change the behavior of the message router.
