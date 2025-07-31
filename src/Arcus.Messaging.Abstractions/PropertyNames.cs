86240ec9457e150539ab04455d5a9b2e9454cfbe
Joachim Goris <gorisjoachim@hotmail.com>
2025-07-31 16:54:18 +0200

    chore(messaging.abstractions): remove propertynames.transactionid

--- src/Arcus.Messaging.Abstractions/PropertyNames.cs
+++ src/Arcus.Messaging.Abstractions/PropertyNames.cs
@@ -16,12 +16,6 @@ namespace Arcus.Messaging.Abstractions
         public const string OperationParentId = "Operation-Parent-Id";
 
         /// <summary>
-        /// Gets the context property name to get the correlation transaction ID.
-        /// </summary>
-        [Obsolete("Will be removed in v3.0 as the property name is only used in the deprecated 'Hierarchical' correlation format")]
-        public const string TransactionId = "Transaction-Id";
-
-        /// <summary>
         /// Gets the context property name to get the encoding that was used on the message.
         /// </summary>
         public const string Encoding = "Message-Encoding";
