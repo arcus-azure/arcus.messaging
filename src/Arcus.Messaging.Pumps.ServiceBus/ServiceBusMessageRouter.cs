fd492d131ad90cb8675b0638c7ea820190153aa7
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-08-26 07:34:23 +0200

    fix(az.sb): add correct az sb context property names

--- src/Arcus.Messaging.Pumps.ServiceBus/ServiceBusMessageRouter.cs
+++ src/Arcus.Messaging.Pumps.ServiceBus/ServiceBusMessageRouter.cs
@@ -213,24 +213,148 @@ namespace Arcus.Messaging.Pumps.ServiceBus
             }
 
             context = context is null ? new Dictionary<string, object>() : new Dictionary<string, object>(context);
-            context["Endpoint"] = serviceBusNamespace;
-            context["EntityName"] = entityName;
-            context["EntityType"] = entityType;
+            context["ServiceBus-Endpoint"] = serviceBusNamespace;
+            context["ServiceBus-EntityName"] = entityName;
+            context["ServiceBus-EntityType"] = entityType;
 
-            logger.LogWarning("@{Request}", new
-            {
-                SourceSystem = RequestSourceSystem.AzureServiceBus,
-                RequestMethod = "<not-applicable>",
-                RequestHost = "<not-applicable>",
-                RequestUri = "<not-applicable>",
-                ResponseStatusCode = isSuccessful ? 200 : 500,
-                OperationName = operationName,
-                RequestDuration = duration,
-                RequestTime = startTime.ToString("yyyy-MM-ddTHH:mm:ss.fffffff zzz"),
-                Context = context
-            });
+            logger.LogWarning("{@Request}", RequestLogEntry.CreateForServiceBus(operationName, isSuccessful, duration, startTime, context));
         }
+        /// <summary>
+        /// Represents a HTTP request as a log entry.
+        /// </summary>
+        private sealed class RequestLogEntry
+        {
+            private RequestLogEntry(
+                string method,
+                string host,
+                string uri,
+                string operationName,
+                int statusCode,
+                RequestSourceSystem sourceSystem,
+                string requestTime,
+                TimeSpan duration,
+                IDictionary<string, object> context)
+            {
+                RequestMethod = method;
+                RequestHost = host;
+                RequestUri = uri;
+                ResponseStatusCode = statusCode;
+                RequestDuration = duration;
+                OperationName = operationName;
+                SourceSystem = sourceSystem;
+                RequestTime = requestTime;
+                Context = context;
+                Context["TelemetryType"] = "Request";
+            }
 
+            /// <summary>
+            /// Creates an <see cref="RequestLogEntry"/> instance for Azure Service Bus requests.
+            /// </summary>
+            /// <param name="operationName">The name of the operation of the request.</param>
+            /// <param name="isSuccessful">The indication whether or not the Azure Service Bus request was successfully processed.</param>
+            /// <param name="duration">The duration it took to process the Azure Service Bus request.</param>
+            /// <param name="startTime">The time when the request was received.</param>
+            /// <param name="context">The telemetry context that provides more insights on the Azure Service Bus request.</param>
+            /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="duration"/> is a negative time range.</exception>
+            public static RequestLogEntry CreateForServiceBus(
+                string operationName,
+                bool isSuccessful,
+                TimeSpan duration,
+                DateTimeOffset startTime,
+                IDictionary<string, object> context)
+            {
+                return CreateWithoutHttpRequest(RequestSourceSystem.AzureServiceBus, operationName, isSuccessful, duration, startTime, context);
+            }
+
+            private static RequestLogEntry CreateWithoutHttpRequest(
+                RequestSourceSystem source,
+                string operationName,
+                bool isSuccessful,
+                TimeSpan duration,
+                DateTimeOffset startTime,
+                IDictionary<string, object> context)
+            {
+                return new RequestLogEntry(
+                    method: "<not-applicable>",
+                    host: "<not-applicable>",
+                    uri: "<not-applicable>",
+                    operationName: operationName,
+                    statusCode: isSuccessful ? 200 : 500,
+                    sourceSystem: source,
+                    requestTime: startTime.ToString("yyyy-MM-ddTHH:mm:ss.fffffff zzz"),
+                    duration: duration,
+                    context: context);
+            }
+
+            /// <summary>
+            /// Gets the HTTP method of the request.
+            /// </summary>
+            public string RequestMethod { get; }
+
+            /// <summary>
+            /// Gets the host that was requested.
+            /// </summary>
+            public string RequestHost { get; }
+
+            /// <summary>
+            /// Gets ths URI of the request.
+            /// </summary>
+            public string RequestUri { get; }
+
+            /// <summary>
+            /// Gets the HTTP response status code that was returned by the service.
+            /// </summary>
+            public int ResponseStatusCode { get; }
+
+            /// <summary>
+            /// Gets the duration of the processing of the request.
+            /// </summary>
+            public TimeSpan RequestDuration { get; }
+
+            /// <summary>
+            /// Gets the date when the request occurred.
+            /// </summary>
+            public string RequestTime { get; }
+
+            /// <summary>
+            /// Gets the type of source system from where the request came from.
+            /// </summary>
+            public RequestSourceSystem SourceSystem { get; set; }
+
+            /// <summary>
+            /// Gets the name of the operation of the source system from where the request came from.
+            /// </summary>
+            public string OperationName { get; }
+
+            /// <summary>
+            /// Gets the context that provides more insights on the HTTP request that was tracked.
+            /// </summary>
+            public IDictionary<string, object> Context { get; }
+
+            /// <summary>
+            /// Returns a string that represents the current object.
+            /// </summary>
+            /// <returns>A string that represents the current object.</returns>
+            public override string ToString()
+            {
+                var contextFormatted = $"{{{String.Join("; ", Context.Select(item => $"[{item.Key}, {item.Value}]"))}}}";
+
+                string source = DetermineSource();
+                bool isSuccessful = ResponseStatusCode is 200;
+
+                return $"{source} from {OperationName} completed in {RequestDuration} at {RequestTime} - (IsSuccessful: {isSuccessful}, Context: {contextFormatted})";
+            }
+
+            private string DetermineSource()
+            {
+                switch (SourceSystem)
+                {
+                    case RequestSourceSystem.AzureServiceBus: return "Azure Service Bus";
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(SourceSystem), "Cannot determine request source as it represents something outside the bounds of the enumeration");
+                }
+            }
+        }
         private enum RequestSourceSystem { AzureServiceBus }
     }
 }
